34207aa77e3906c7dac92cf7c38fbd1f3bf09b32
rhiskey <first3395@gmail.com>
2024-07-29 16:53:12 +0300

    upd

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'MirrorSoul';
+export const SITE_DESCRIPTION = 'Game Dev Blog';
+export const BASE = '/mirrorsoul-blog';
