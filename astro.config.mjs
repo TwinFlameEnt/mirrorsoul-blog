1f57e12247502bc40f54c31b6603046658462b5d
rhiskey <first3395@gmail.com>
2024-07-29 16:41:20 +0300

    page fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://twinflameent.github.io/mirrorsoul-blog/',
-	base: '/mirrorsoul-blog',
+	site: 'https://twinflameent.github.io',
+	base: 'mirrorsoul-blog',
 	integrations: [mdx(), sitemap()],
 });
